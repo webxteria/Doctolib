0c6f4112da91bf9b268b7c21cb42fa0545e662fb
Muhammad Faran <fa@baernholdt.com>
2019-09-14 13:18:37 +0500

    Test cases fixed, Refactored getAvailabilities.js

--- src/getAvailabilities.js
+++ src/getAvailabilities.js
@@ -1,39 +1,67 @@
-import moment from "moment";
-import knex from "knexClient";
+import moment from 'moment';
+import knex from 'knexClient';
 
-export default async function getAvailabilities(date) {
-  const availabilities = new Map();
-  for (let i = 0; i < 7; ++i) {
-    const tmpDate = moment(date).add(i, "days");
-    availabilities.set(tmpDate.format("d"), {
-      date: tmpDate.toDate(),
-      slots: []
+export default async function getAvailabilities(date, numberOfDays = 7) {
+
+  let result = [];
+  let weeks = Math.ceil(numberOfDays / 7);
+
+  for (let j = 0; j < weeks; j++) {
+
+    let dayCount = 7;
+    if (j + 1 === weeks) dayCount = numberOfDays % 7; 
+    if (numberOfDays % 7 === 0) dayCount = 7;
+    const availabilities = new Map();
+
+    for (let i = 0; i < dayCount; ++i) {
+      const tmpDate = moment(date).add(i, 'days', j, 'weeks');
+      availabilities.set(tmpDate.format('d'), {
+        date: tmpDate.toDate(),
+        slots: []
+      });
+    }
+
+    const events = await knex
+      .select('kind', 'starts_at', 'ends_at', 'weekly_recurring')
+      .from('events')
+      .where(function() {
+        this.where('weekly_recurring', true).orWhere('ends_at', '>', +date);
+      });
+
+    const openings = events.filter(el => {
+      return el.kind === 'opening';
     });
-  }
 
-  const events = await knex
-    .select("kind", "starts_at", "ends_at", "weekly_recurring")
-    .from("events")
-    .where(function() {
-      this.where("weekly_recurring", true).orWhere("ends_at", ">", +date);
+    const nonOpenings = events.filter(el => {
+      return el.kind !== 'opening';
     });
 
-  for (const event of events) {
-    for (
-      let date = moment(event.starts_at);
-      date.isBefore(event.ends_at);
-      date.add(30, "minutes")
-    ) {
-      const day = availabilities.get(date.format("d"));
-      if (event.kind === "opening") {
-        day.slots.push(date.format("H:mm"));
-      } else if (event.kind === "appointment") {
+    for (const opening of openings) {
+      for (
+        let date = moment(opening.starts_at);
+        date.isBefore(opening.ends_at);
+        date.add(30, 'minutes')
+      ) {
+        const day = availabilities.get(date.format('d'));
+
+        if (opening.starts_at - day.date.getTime() < 86400000)
+          day.slots.push(date.format('H:mm'));
+      }
+    }
+
+    for (const nonOpening of nonOpenings) {
+      for (
+        let date = moment(nonOpening.starts_at);
+        date.isBefore(nonOpening.ends_at);
+        date.add(30, 'minutes')
+      ) {
+        const day = availabilities.get(date.format('d'));
         day.slots = day.slots.filter(
-          slot => slot.indexOf(date.format("H:mm")) === -1
+          slot => slot.indexOf(date.format('H:mm')) === -1
         );
       }
     }
+    result = result.concat(Array.from(availabilities.values()));
   }
-
-  return Array.from(availabilities.values())
+  return result;
 }
--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -1,91 +1,181 @@
-import knex from "knexClient";
-import getAvailabilities from "./getAvailabilities";
+import knex from 'knexClient';
+import getAvailabilities from './getAvailabilities';
 
-describe("getAvailabilities", () => {
-  beforeEach(() => knex("events").truncate());
+describe('getAvailabilities', () => {
+  beforeEach(() => knex('events').truncate());
 
-  describe("case 1", () => {
-    it("test 1", async () => {
-      const availabilities = await getAvailabilities(new Date("2014-08-10"));
+  describe('case 1', () => {
+    it('test 1', async () => {
+      let availabilities = await getAvailabilities(new Date('2014-08-10'));
       expect(availabilities.length).toBe(7);
       for (let i = 0; i < 7; ++i) {
         expect(availabilities[i].slots).toEqual([]);
       }
+      
+      availabilities = await getAvailabilities(new Date('2014-08-10'),2);
+      expect(availabilities.length).toBe(2);
+
+      availabilities = await getAvailabilities(new Date('2014-08-10'),10);
+      expect(availabilities.length).toBe(10);
+
+      availabilities = await getAvailabilities(new Date('2014-08-10'),20);
+      expect(availabilities.length).toBe(20);
     });
   });
 
-  describe("case 2", () => {
+  describe('case 2', () => {
     beforeEach(async () => {
-      await knex("events").insert([
+      await knex('events').insert([
         {
-          kind: "appointment",
-          starts_at: new Date("2014-08-11 10:30"),
-          ends_at: new Date("2014-08-11 11:30")
+          kind: 'appointment',
+          starts_at: new Date('2014-08-11 10:30'),
+          ends_at: new Date('2014-08-11 11:30')
         },
         {
-          kind: "opening",
-          starts_at: new Date("2014-08-04 09:30"),
-          ends_at: new Date("2014-08-04 12:30"),
+          kind: 'opening',
+          starts_at: new Date('2014-08-04 09:30'),
+          ends_at: new Date('2014-08-04 12:30'),
           weekly_recurring: true
         }
       ]);
     });
 
-    it("test 1", async () => {
-      const availabilities = await getAvailabilities(new Date("2014-08-10"));
+    it('test 1', async () => {
+      const availabilities = await getAvailabilities(new Date('2014-08-10'));
       expect(availabilities.length).toBe(7);
 
       expect(String(availabilities[0].date)).toBe(
-        String(new Date("2014-08-10"))
+        String(new Date('2014-08-10'))
       );
       expect(availabilities[0].slots).toEqual([]);
 
       expect(String(availabilities[1].date)).toBe(
-        String(new Date("2014-08-11"))
+        String(new Date('2014-08-11'))
       );
       expect(availabilities[1].slots).toEqual([
-        "9:30",
-        "10:00",
-        "11:30",
-        "21:00"
+        '9:30',
+        '10:00',
+        '11:30',
+        '12:00'
       ]);
 
-      expect(String(availabilities[6].date)).toBe(
-        String(new Date("2014-08-16"))
+      expect(String(availabilities[2].date)).toBe(
+        String(new Date('2014-08-12'))
       );
+      expect(availabilities[2].slots).toEqual([]);
+
+      expect(String(availabilities[3].date)).toBe(
+        String(new Date('2014-08-13'))
+      );
+      expect(availabilities[3].slots).toEqual([]);
+
+      expect(String(availabilities[4].date)).toBe(
+        String(new Date('2014-08-14'))
+      );
+      expect(availabilities[4].slots).toEqual([]);
+
+      expect(String(availabilities[5].date)).toBe(
+        String(new Date('2014-08-15'))
+      );
+      expect(availabilities[5].slots).toEqual([]);
+
+      expect(String(availabilities[6].date)).toBe(
+        String(new Date('2014-08-16'))
+      );
+
+      expect(availabilities[6].slots).toEqual([]);
     });
   });
 
-  describe("case 3", () => {
+  describe('case 3', () => {
     beforeEach(async () => {
-      await knex("events").insert([
+      await knex('events').insert([
         {
-          kind: "appointment",
-          starts_at: new Date("2014-08-11 10:30"),
-          ends_at: new Date("2014-08-11 11:30")
+          kind: 'appointment',
+          starts_at: new Date('2014-08-11 10:30'),
+          ends_at: new Date('2014-08-11 11:30')
         },
         {
-          kind: "opening",
-          starts_at: new Date("2018-08-04 09:30"),
-          ends_at: new Date("2018-08-04 12:30"),
+          kind: 'opening',
+          starts_at: new Date('2018-08-04 09:30'),
+          ends_at: new Date('2018-08-04 12:30'),
           weekly_recurring: true
         }
       ]);
     });
 
-    it("test 1", async () => {
-      const availabilities = await getAvailabilities(new Date("2014-08-10"));
+    it('test 1', async () => {
+      const availabilities = await getAvailabilities(new Date('2014-08-10'));
       expect(availabilities.length).toBe(7);
 
       expect(String(availabilities[0].date)).toBe(
-        String(new Date("2014-08-10"))
+        String(new Date('2014-08-10'))
       );
       expect(availabilities[0].slots).toEqual([]);
 
       expect(String(availabilities[1].date)).toBe(
-        String(new Date("2014-08-11"))
+        String(new Date('2014-08-11'))
       );
       expect(availabilities[6].slots).toEqual([]);
+    });
+  });
+
+  describe('case 4', () => {
+    beforeEach(async () => {
+      await knex('events').insert([
+        {
+          kind: 'appointment',
+          starts_at: new Date('2018-12-05 9:00'),
+          ends_at: new Date('2018-12-05 10:00')
+        },
+        {
+          kind: 'opening',
+          starts_at: new Date('2018-12-03 09:30'),
+          ends_at: new Date('2018-12-03 12:00'),
+          weekly_recurring: true
+        },
+        {
+          kind: 'opening',
+          starts_at: new Date('2018-12-04 09:30'),
+          ends_at: new Date('2018-12-04 12:00'),
+          weekly_recurring: true
+        },
+        {
+          kind: 'opening',
+          starts_at: new Date('2018-12-05 09:30'),
+          ends_at: new Date('2018-12-05 12:00'),
+          weekly_recurring: true
+        },
+        {
+          kind: 'opening',
+          starts_at: new Date('2018-12-06 09:30'),
+          ends_at: new Date('2018-12-06 12:00'),
+          weekly_recurring: true
+        },
+        {
+          kind: 'appointment',
+          starts_at: new Date('2018-12-06 0:00'),
+          ends_at: new Date('2018-12-06 24:00')
+        } 
+      ]);
+    });
+
+    it('test 1', async () => {
+      const availabilities = await getAvailabilities(new Date('2018-12-03'));
+
+      expect(availabilities.length).toBe(7);
+
+      expect(String(availabilities[0].date)).toBe(
+        String(new Date('2018-12-03'))
+      );
+
+      expect(availabilities[2].slots).toEqual(['10:00','10:30', '11:00', '11:30']);
+
+      expect(availabilities[3].slots).toEqual([]);
+
+      expect(String(availabilities[6].date)).toBe(
+        String(new Date('2018-12-09'))
+      );
     });
   });
 });
